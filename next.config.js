afff604cd103b10019e6458d54db89bee07d7de4
Aman Dubey <dubeyaman905@gmail.com>
2023-09-19 01:40:45 +0530

    update config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
     images: {
         domains: ["lh3.googleusercontent.com", "firebasestorage.googleapis.com"],
     },
+    env: {
+        DATABASE_URL: process.env.DATABASE_URL,
+      },
 }
 
 module.exports = nextConfig
