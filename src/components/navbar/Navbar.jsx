3330711b5426b50794f8d050b9991abc6b0e36f4
Aman Dubey <dubeyaman905@gmail.com>
2023-09-20 21:11:08 +0530

    nav

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -19,26 +19,16 @@ const Navbar = () => {
             <p>&nbsp;𝐏𝐞𝐧𝐂𝐡𝐚𝐩𝐭𝐞𝐫𝐬</p>
           </Link>
         </div>
-        {/* <div className={styles.social}>
-          <Image src="/facebook.png" alt="facebook" width={24} height={24} />
-          <Image src="/instagram.png" alt="instagram" width={24} height={24} />
-          <Image src="/tiktok.png" alt="tiktok" width={24} height={24} />
-          <Image src="/youtube.png" alt="youtube" width={24} height={24} />
-        </div> */}
         <div className={styles.links}>
           <div className={styles.link}>
-
-            <Button value="Home" href="/#home" ><ImHome /> </Button>
-            {/* <Link href="/" className={styles.link}>Home</Link> */}
+            <Button value="Home" href="/" ><ImHome /> </Button>
           </div>
           <div className={styles.link}>
             <Button value="Category" href="/#category" ><BiCategory /> </Button>
-            {/* <Link href="/" className={styles.link}>Category</Link> */}
           </div>
           <div className={styles.link}>
 
             <Button value="About" href="/#footer"><GrContactInfo /> </Button>
-            {/* <Link href="/" className={styles.link}>About</Link> */}
           </div>
           <AuthLinks />
           <ThemeToggle />
